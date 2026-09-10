5f1f4db75c837b048463db66aa35342213f4b805
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-10 17:26:00 +0300

    feat: enhance auth configuration with drizzleAdapter and session management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,24 @@
-import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { betterAuth } from "better-auth/minimal";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
+import { db } from "../db";
+import { schema } from "../db/schema";
+
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [tanstackStartCookies()],
+	database: drizzleAdapter(db, {
+		provider: "pg",
+		schema,
+		usePlural: true,
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [tanstackStartCookies()],
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 5 * 60,
+		},
+	},
 });
