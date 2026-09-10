7faac0aefdeded50bfcf96fae4f44f19a13081dc
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-10 17:33:25 +0300

    feat: refactor database initialization with connection pooling and enhanced logging

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,14 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-import * as schema from "./schema.ts";
+import { authRelations, relations } from "./relations";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({
+	client: pool,
+	logger: true,
+	relations: { ...relations, ...authRelations },
+});
