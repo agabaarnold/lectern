f8d45a141a0e42de4148713a341944d06918a2e7
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-11 19:23:24 +0300

    feat: add FormInput component for text input handling

---
+++ src/components/form/form-input.tsx
@@ -0,0 +1,40 @@
+import { useFieldContext } from "#/hooks/form/use-form-context.ts";
+
+import { Field, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
+
+interface FormInputProps {
+	label: string;
+	placeholder: string;
+	type: Exclude<
+		React.ComponentProps<"input">["type"],
+		"password" | "checkbox" | "number"
+	>;
+}
+
+const FormInput = ({ label, placeholder, type }: FormInputProps) => {
+	const field = useFieldContext<string>();
+	const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+
+	return (
+		<Field data-invalid={isInvalid}>
+			<FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+
+			<Input
+				aria-invalid={isInvalid}
+				id={field.name}
+				name={field.name}
+				onBlur={(e) => {
+					field.handleChange(e.target.value.trim());
+					field.handleBlur();
+				}}
+				onChange={(e) => field.handleChange(e.target.value)}
+				placeholder={placeholder}
+				type={type}
+				value={field.state.value}
+			/>
+		</Field>
+	);
+};
+
+export default FormInput;
