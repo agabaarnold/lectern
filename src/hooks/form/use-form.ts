c74a918b1e61147da018fbbc4f397ab91b450db2
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-12 01:35:18 +0300

    feat: create useAppForm hook with form components integration

---
+++ src/hooks/form/use-form.ts
@@ -0,0 +1,15 @@
+import { createFormHook } from "@tanstack/react-form-start";
+
+import FormCheckbox from "#/components/form/form-checkbox.tsx";
+import FormInput from "#/components/form/form-input.tsx";
+import FormPassword from "#/components/form/form-password.tsx";
+import FormSelect from "#/components/form/form-select.tsx";
+
+import { fieldContext, formContext } from "./use-form-context";
+
+export const { useAppForm } = createFormHook({
+	fieldComponents: { FormInput, FormCheckbox, FormPassword, FormSelect },
+	fieldContext,
+	formComponents: {},
+	formContext,
+});
