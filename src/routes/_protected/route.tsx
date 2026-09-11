6a1ffa3e60e62597868c7f16f34b460f1ec69fa6
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-12 02:00:09 +0300

    feat: add protected route with session validation

---
+++ src/routes/_protected/route.tsx
@@ -0,0 +1,20 @@
+// oxlint-disable react/function-component-definition func-style
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+
+import { getUserSession } from "#/features/auth/functions/index.ts";
+
+export const Route = createFileRoute("/_protected")({
+	component: RouteComponent,
+	beforeLoad: async ({ location }) => {
+		const { session } = await getUserSession();
+		if (!session) {
+			throw redirect({ to: "/login", search: { redirect: location.href } });
+		}
+
+		return { user: session.user };
+	},
+});
+
+function RouteComponent() {
+	return <Outlet />;
+}
