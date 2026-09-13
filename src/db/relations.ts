6f8983746892ab4e33fefda336ab61fdacc88e59
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-13 12:32:05 +0300

    feat: enhance authentication relations by adding organizations, members, and invitations

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -1,17 +1,45 @@
 import { defineRelations, defineRelationsPart } from "drizzle-orm";
 
 import { schema } from "./schema";
-import { accounts, sessions, users, verifications } from "./schema/auth.schema";
+import {
+	accounts,
+	invitations,
+	members,
+	organizations,
+	sessions,
+	users,
+	verifications,
+} from "./schema/auth.schema";
 
 export const relations = defineRelations(schema, () => ({}));
 
 export const authRelations = defineRelationsPart(
-	{ accounts, sessions, users, verifications },
+	{
+		users,
+		sessions,
+		accounts,
+		verifications,
+		organizations,
+		members,
+		invitations,
+	},
 	(r) => ({
-		accounts: {
-			user: r.one.users({
-				from: r.accounts.userId,
-				to: r.users.id,
+		users: {
+			sessions: r.many.sessions({
+				from: r.users.id,
+				to: r.sessions.userId,
+			}),
+			accounts: r.many.accounts({
+				from: r.users.id,
+				to: r.accounts.userId,
+			}),
+			members: r.many.members({
+				from: r.users.id,
+				to: r.members.userId,
+			}),
+			invitations: r.many.invitations({
+				from: r.users.id,
+				to: r.invitations.inviterId,
 			}),
 		},
 		sessions: {
@@ -20,14 +48,40 @@ export const authRelations = defineRelationsPart(
 				to: r.users.id,
 			}),
 		},
-		users: {
-			accounts: r.many.accounts({
-				from: r.users.id,
-				to: r.accounts.userId,
+		accounts: {
+			user: r.one.users({
+				from: r.accounts.userId,
+				to: r.users.id,
 			}),
-			sessions: r.many.sessions({
-				from: r.users.id,
-				to: r.sessions.userId,
+		},
+		organizations: {
+			members: r.many.members({
+				from: r.organizations.id,
+				to: r.members.organizationId,
+			}),
+			invitations: r.many.invitations({
+				from: r.organizations.id,
+				to: r.invitations.organizationId,
+			}),
+		},
+		members: {
+			organization: r.one.organizations({
+				from: r.members.organizationId,
+				to: r.organizations.id,
+			}),
+			user: r.one.users({
+				from: r.members.userId,
+				to: r.users.id,
+			}),
+		},
+		invitations: {
+			organization: r.one.organizations({
+				from: r.invitations.organizationId,
+				to: r.organizations.id,
+			}),
+			user: r.one.users({
+				from: r.invitations.inviterId,
+				to: r.users.id,
 			}),
 		},
 	})
