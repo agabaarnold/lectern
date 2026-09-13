8245153fcd89f5f6a4246427eb71a94b64259823
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-13 14:26:23 +0300

    feat: enhance auth client by adding organization client to plugins

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,8 @@
-import { inferAdditionalFields } from "better-auth/client/plugins";
+import { inferAdditionalFields, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-	plugins: [inferAdditionalFields<typeof auth>()],
+	plugins: [organizationClient(), inferAdditionalFields<typeof auth>()],
 });
