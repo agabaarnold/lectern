0d18ab77b4acb503854a1eee24c7cc855cfe960c
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-13 11:28:05 +0300

    feat: enhance router setup with default error and not found components

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,41 @@
+// oxlint-disable func-style
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+
+import ErrorComponent from "#/components/errors/error-component.tsx";
+import NotFoundComponent from "#/components/errors/not-found-component.tsx";
 
 import { getContext } from "./integrations/tanstack-query/root-provider";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  const context = getContext();
+	const context = getContext();
 
-  const router = createTanStackRouter({
-    routeTree,
-    context,
-    scrollRestoration: true,
-    defaultPreload: "intent",
-    defaultPreloadStaleTime: 0,
-  });
+	const router = createTanStackRouter({
+		routeTree,
+		context,
+		defaultErrorComponent: ({ error, reset, info }) => (
+			<ErrorComponent error={error} reset={reset} info={info} />
+		),
+		defaultNotFoundComponent: ({ isNotFound, routeId, data }) => (
+			<NotFoundComponent
+				data={data}
+				isNotFound={isNotFound}
+				routeId={routeId}
+			/>
+		),
+		scrollRestoration: true,
+		defaultPreload: "intent",
+		defaultPreloadStaleTime: 0,
+	});
 
-  setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient });
+	setupRouterSsrQueryIntegration({ router, queryClient: context.queryClient });
 
-  return router;
+	return router;
 }
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: ReturnType<typeof getRouter>;
-  }
+	interface Register {
+		router: ReturnType<typeof getRouter>;
+	}
 }
