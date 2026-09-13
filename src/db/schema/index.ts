10956093b5b936301295cabae239735d9fac5cb2
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-13 12:31:56 +0300

    feat: expand schema to include invitations, members, and organizations

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,21 @@
-import { accounts, sessions, users, verifications } from "./auth.schema";
+import {
+	accounts,
+	invitations,
+	members,
+	organizations,
+	sessions,
+	users,
+	verifications,
+} from "./auth.schema";
 
-const schema = { accounts, sessions, users, verifications } as const;
+const schema = {
+	accounts,
+	invitations,
+	members,
+	organizations,
+	sessions,
+	users,
+	verifications,
+} as const;
 
 export { schema };
